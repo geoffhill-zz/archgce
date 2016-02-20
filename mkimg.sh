24c7317f6b37770b5ac7db8a258a9631a413588a
Geoff Hill <geoff@geoffhill.org>
2016-02-20 15:48:26 -0800

    mkimg produces image that testimg can run.
    At this point, QEMU can run an output disk image using the testimg
    script.

--- mkimg.sh
+++ mkimg.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-HOSTNAME="${HOSTNAME:-arch.gfrh.net}"
+HNAME="${HNAME:-arch.gfrh.net}"
+USERKEY=/home/geoff/.ssh/id_rsa.pub
 SIZE="${SIZE:-4G}"
 
-set -e
+randpass() {
+    cat /dev/urandom | tr -dc "A-Fa-f0-9" | fold -w 32 | head -n 1
+}
 
 # Ensure the required utilities are installed.
 pacman -Q util-linux
 pacman -Q arch-install-scripts
 pacman -Q syslinux
 
-IMG=root.fs
+IMG=disk.raw
 rm -f $IMG
 truncate -s $SIZE $IMG
+chmod 666 $IMG
 sfdisk $IMG <<'EOF'
 label: dos
 start=2048,size=128M,type=83,bootable
@@ -35,18 +39,29 @@ mkdir $BOOT
 mount $BOOTDEV $BOOT
 
 set +e
+pacstrap -c -M $ROOT base syslinux openssh ntp sudo mg git tmux
+set -e
 
-pacstrap -c -M $ROOT base base-devel syslinux openssh sudo mg
+cat >> $ROOT/etc/fstab <<'EOF'
+/dev/sda2  /       ext4    rw,relatime,data=ordered    0 1
+/dev/sda1  /boot   ext4    rw,relatime,data=ordered    0 2
+EOF
 
-echo $HOSTNAME > $ROOT/etc/hostname
+sed -i "s#root=[^ ]*#root=/dev/sda2 loglevel=5#g" $ROOT/boot/syslinux/syslinux.cfg
+sed -i "s#DEFAULT arch#DEFAULT archfallback#" $ROOT/boot/syslinux/syslinux.cfg
+
+echo $HNAME > $ROOT/etc/hostname
 ln -s ../usr/share/zoneinfo/UTC $ROOT/etc/localtime
 echo en_US.UTF-8 UTF-8 >> $ROOT/etc/locale.gen
 echo "LANG=en_US.UTF-8" > $ROOT/etc/locale.conf
 
-cat >> $ROOT/etc/pacman.d/mirrorlist <<'EOF'
+cat > $ROOT/etc/pacman.d/mirrorlist <<'EOF'
 Server = http://lug.mtu.edu/archlinux/$repo/os/$arch
 Server = http://mirror.rit.edu/archlinux/$repo/os/$arch
 EOF
+
+sed -i '/server /d' $ROOT/etc/ntp.conf
+echo server 169.254.169.254 iburst >> $ROOT/etc/ntp.conf
 
 cat > $ROOT/etc/iptables/iptables.rules <<'EOF'
 *filter
@@ -68,16 +83,35 @@ cat > $ROOT/etc/iptables/iptables.rules <<'EOF'
 COMMIT
 EOF
 
+# GCE times out TCP connections longer than 10m.
+echo ServerAliveInterval 450 >> $ROOT/etc/ssh/ssh_config
+echo ClientAliveInterval 450 >> $ROOT/etc/ssh/sshd_config
+
 echo "geoff ALL=(ALL) ALL" >> $ROOT/etc/sudoers
 syslinux-install_update -i -a -m -c $ROOT/
 
 arch-chroot $ROOT <<'EOF'
 locale-gen
-systemctl enable dhcpcd@enp1s0
+systemctl enable dhcpcd.service
 systemctl enable sshd.service
 systemctl enable iptables.service
+systemctl enable ntpd.service
+pacman -Syy
+useradd --shell /bin/bash --create-home geoff
+mkinitcpio -p linux
+#usermod -L root
 EOF
 
+SSHDIR=$ROOT/home/geoff/.ssh
+mkdir $SSHDIR
+chmod 700 $SSHDIR
+cp $USERKEY $SSHDIR/authorized_keys
+chmod 600 $SSHDIR/authorized_keys
+chown -R 1000:1000 $SSHDIR
+
 umount -R $ROOT
-losetup -d $ROOTDEV
-losetup -d $BOOTDEV
+sync
+losetup -d $LOOPDEV
+sync
+
+rmdir $ROOT
--- testimg.sh
+++ testimg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qemu-system-x86_64 --enable-kvm -smp 1 -m 2048M -net nic,model=virtio -net user -device virtio-scsi-pci,id=scsi -device scsi-hd,drive=hd,physical_block_size=4096 -drive if=none,id=hd,file=root.fs,cache=none
+qemu-system-x86_64 --enable-kvm -smp 1 -m 2048M -net nic,model=virtio -net user -redir tcp:5555::22 -device virtio-scsi-pci,id=scsi -device scsi-hd,drive=hd,physical_block_size=4096 -drive if=none,id=hd,file=disk.raw,format=raw,cache=none
